fd85bbf5dc57dd375df24dbb6138db5b15707a52
charu ranjana <charuranjana@Host-006.homenet.telecomitalia.it>
2025-02-14 23:30:25 +0100

    Added video chat link to homepage

--- video.js
+++ video.js
@@ -1,6 +1,5 @@
 const socket = io("https://anonymous-chat-backend-jquo.onrender.com");
 let localStream, peerConnection;
-
 const servers = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
 const startCallBtn = document.getElementById('startCall');
 const endCallBtn = document.getElementById('endCall');
@@ -11,11 +10,9 @@ startCallBtn.addEventListener('click', async () => {
   localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   localVideo.srcObject = localStream;
   peerConnection = new RTCPeerConnection(servers);
-
   localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
   peerConnection.onicecandidate = e => e.candidate && socket.emit('ice', e.candidate);
   peerConnection.ontrack = e => remoteVideo.srcObject = e.streams[0];
-
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
   socket.emit('offer', offer);
